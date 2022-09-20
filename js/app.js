241a49d84330258edb1c79e2ba4e5a2c86312a21
DiegoVelasco <20193tn054@utez.edu.mx>
2022-09-20 14:02:56 -0500

    [UPDATE] Update file app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if(navigator.serviceWorker){
     console.log("Si lo soporta");
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('./sw.js');
 }else{
     console.log("No lo soporta")
 }
